70c2b39fe5ec54e6e061f112dcf4d9b84acd477d
Warner Nielsen <bwn888@gmail.com>
2017-03-24 13:59:42 -0600

    Wrote this while following tutorial and now uploading files to github

---
+++ seeds/todo.js
@@ -0,0 +1,26 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('todo').del()
+    .then(function () {
+      const todos = [{
+        title: 'Build a CRUD App',
+        priority: 1,
+        date: new Date()
+      }, {
+        title: 'Do the dishes',
+        priority: 3,
+        date: new Date()
+      }, {
+        title: 'Render a view',
+        priority: 2,
+        date: new Date()
+      }, {
+        title: 'Walk the dog',
+        priority: 5,
+        date: new Date()
+      }];
+
+      return knex('todo').insert(todos);
+    });
+};
